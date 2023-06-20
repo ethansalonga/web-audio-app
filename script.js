257aeab0eb077ecab4cfe87820ab42b4619c3ae0
Ethan Salonga <ethansalonga28@gmail.com>
2023-06-20 16:10:07 -0400

    Setup visualizer

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const treble = document.getElementById("treble")
 const visualizer = document.getElementById("visualizer")
 
 const context = new AudioContext()
+const analyserNode = new AnalyserNode(context, { fftSize: 256 })
 
 setupContext()
+resize()
+drawVisualizer()
 
 async function setupContext() {
   const guitar = await getGuitar()
@@ -14,7 +17,7 @@ async function setupContext() {
     await context.resume()
   }
   const source = context.createMediaStreamSource(guitar)
-  source.connect(context.destination)
+  source.connect(analyserNode).connect(context.destination)
 }
 
 function getGuitar() {
@@ -26,4 +29,31 @@ function getGuitar() {
       latency: 0,
     },
   })
+}
+
+function drawVisualizer() {
+  requestAnimationFrame(drawVisualizer)
+
+  const bufferLength = analyserNode.frequencyBinCount
+  const dataArray = new Uint8Array(bufferLength)
+  analyserNode.getByteFrequencyData(dataArray)
+  const width = visualizer.width
+  const height = visualizer.height
+  const barWidth = width / bufferLength
+
+  const canvasContext = visualizer.getContext("2d")
+  canvasContext.clearRect(0, 0, width, height)
+
+  dataArray.forEach((item, index) => {
+    const y = ((item / 255) * height) / 2
+    const x = barWidth * index
+
+    canvasContext.fillStyle = `hsl(${(y / height) * 400}, 100%, 50%)`
+    canvasContext.fillRect(x, height - y, barWidth, y)
+  })
+}
+
+function resize() {
+  visualizer.width = visualizer.clientWidth * window.devicePixelRatio
+  visualizer.height = visualizer.clientHeight * window.devicePixelRatio
 }
